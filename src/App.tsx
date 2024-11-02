c105534929c0a987c0b17f357243e3800292ef63
Luke <lukeweston1234@gmail.com>
2024-11-02 14:44:52 -0400

    some layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./shared/ui/header/header";
 import Lighting from "./shared/ui/lighting/lighting";
 
 export default function App() {
   return (
-    <div class="w-screen h-screen bg-app-background">
+    <div class="w-screen h-screen bg-app-background p-6 overflow-hidden">
       <Lighting />
+      <Header />
       <h1 class="text-3xl font-bold underline text-app-primary">
         Hello world!
       </h1>
