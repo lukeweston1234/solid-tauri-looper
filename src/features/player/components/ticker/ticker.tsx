c46cea96a5655f3d3e58f34d5864b67ddf43de56
Luke <lukeweston1234@gmail.com>
2024-11-07 22:24:34 -0500

    header state tweaks

--- src/features/player/components/ticker/ticker.tsx
+++ src/features/player/components/ticker/ticker.tsx
@@ -1,11 +1,19 @@
-import { createSignal } from "solid-js";
+import { createSignal, onCleanup } from "solid-js";
+import { useAppContext } from "../../../../core/app-state/app.context";
 
 export default function Ticker() {
+  const [state] = useAppContext();
   const [position, setPosition] = createSignal(0);
 
-  setInterval(() => {
-    setPosition((prev) => (prev >= 100 ? 0 : prev + 0.2));
+  const tickerInterval = setInterval(() => {
+    if (state.status === "playing" || state.status === "recording") {
+      setPosition((prev) => (prev >= 100 ? 0 : prev + 0.2));
+    } else if (state.status === "stopped") {
+      setPosition(0);
+    }
   }, 10);
+
+  onCleanup(() => clearInterval(tickerInterval));
 
   return (
     <div class="w-full h-full relative">
